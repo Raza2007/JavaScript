824248088ef8f0e553252e64048a0b2d1b863ed9
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-15 19:50:39 +0000

    How to declare a const var let

---
+++ first/Four.js
@@ -0,0 +1,20 @@
+
+const accountId =125874
+
+let accountEmail ="ahdraza512@gmail.com"
+
+var accountPassword ="12345"
+accountCity="Mumbai"
+let accountState;
+
+// accountId=2  not allowed
+
+
+
+
+accountEmail ="abd@512gmail.com"
+accountPassword="58258"
+accountCity="Gonda"
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
